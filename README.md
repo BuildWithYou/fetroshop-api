1fc217b13bb80dc554ecff2d2e64e1e042d14afe
etwicaksono <ekoteguhwicaksono@gmail.com>
2023-12-28 21:57:41 +0700

    Add swagger ui

---
+++ README.md
@@ -0,0 +1,4 @@
+# FETROSHOP API
+
+###### GENERATE DOCS FILE
+`go-swagger3 --module-path . --output ./docs/OpenAPI3/source/fetroshop-api.json --schema-without-pkg`
--- main.go
+++ main.go
@@ -15,6 +15,19 @@ import (
 	"github.com/spf13/viper"
 )
 
+// @Version 1.0.0
+// @Title Backend API
+// @Description API usually works as expected. But sometimes its not true.
+// @ContactName Parvez
+// @ContactEmail abce@email.com
+// @ContactURL http://someurl.oxox
+// @TermsOfServiceUrl http://someurl.oxox
+// @LicenseName MIT
+// @LicenseURL https://en.wikipedia.org/wiki/MIT_License
+// @Server http://www.fake.com Server-1
+// @Server http://www.fake2.com Server-2
+// @Security AuthorizationHeader read write
+// @SecurityScheme AuthorizationHeader http bearer Input your token
 func main() {
 	config := viper.New()
 	config.SetConfigFile("config.json")
@@ -45,6 +58,9 @@ func main() {
 		Registration: registrationController,
 	}
 	router.ApiRoutes(fiberApp)
+
+	// Swagger
+	fiberApp.Static("/docs", "docs/OpenAPI3")
 
 	// Middleware
 	middleware.NotFoundMiddleware(fiberApp) // 404 Handler
