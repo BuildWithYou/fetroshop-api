3c5e8de6244b705917fd5077cd56975e7f8edef9
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-01-05 14:11:09 +0700

    CMS: Setup user repository

--- app/domain/users/postgres/create.go
+++ app/domain/users/postgres/create.go
@@ -1,7 +1,10 @@
 package postgres
 
-import "github.com/BuildWithYou/fetroshop-api/app/domain/users"
+import (
+	"github.com/BuildWithYou/fetroshop-api/app/domain/users"
+	"gorm.io/gorm"
+)
 
-func (p *PostgreSQL) Create(user *users.User) error {
-	return nil
+func (p *PostgreSQL) Create(user *users.User) *gorm.DB {
+	return p.DB.Create(user)
 }
