bdc8e935bcbefb0a37f75d243158cd4de598c178
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-01-08 12:50:59 +0700

    Boilerplate: update database connection provider

--- app/domain/customer_accesses/postgres/update-or-create_test.go
+++ app/domain/customer_accesses/postgres/update-or-create_test.go
@@ -7,10 +7,10 @@ import (
 	"github.com/BuildWithYou/fetroshop-api/app/connection"
 	"github.com/BuildWithYou/fetroshop-api/app/domain/customer_accesses"
 	"github.com/BuildWithYou/fetroshop-api/app/helper/confighelper"
-	"gorm.io/gorm"
+	"github.com/BuildWithYou/fetroshop-api/app/helper/validatorhelper"
 )
 
-var dbConnection = connection.OpenDBConnection(confighelper.GetConfig())
+var dbConnection, err = connection.OpenDBConnection(connection.DB_TEST, confighelper.GetConfig())
 
 func TestPostgreSQLUpdateOrCreate(t *testing.T) {
 	type args struct {
@@ -21,18 +21,31 @@ func TestPostgreSQLUpdateOrCreate(t *testing.T) {
 		name string
 		p    *PostgreSQL
 		args args
-		want *gorm.DB
+		want any
 	}{
 		{
 			name: "UpdateOrCreate",
 			p:    &PostgreSQL{DB: dbConnection},
 			args: args{
-				data:      &customer_accesses.CustomerAccess{},
-				condition: &customer_accesses.CustomerAccess{},
+				data: &customer_accesses.CustomerAccess{
+					Token:      "test-token2",
+					CustomerID: 1,
+					Platform:   "test-platform",
+					UserAgent:  "test-user-agent",
+				},
+				condition: &customer_accesses.CustomerAccess{
+					CustomerID: 1,
+					Platform:   "test-platform",
+					UserAgent:  "test-user-agent"},
 			},
 			want: nil,
 		},
 	}
+
+	if validatorhelper.IsNotNil(err) {
+		t.Errorf("PostgreSQL.UpdateOrCreate() failed. error = %v", err.Error())
+	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := tt.p.UpdateOrCreate(tt.args.data, tt.args.condition); !reflect.DeepEqual(got, tt.want) {
