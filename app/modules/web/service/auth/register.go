54f233664c1f1f4d85072a81409c5cd654fac20e
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-01-17 16:46:19 +0700

    Web: Auth : Refactor register service

--- app/modules/web/service/auth/register.go
+++ app/modules/web/service/auth/register.go
@@ -5,6 +5,7 @@ import (
 	"github.com/BuildWithYou/fetroshop-api/app/helper/errorhelper"
 	"github.com/BuildWithYou/fetroshop-api/app/helper/gormhelper"
 	"github.com/BuildWithYou/fetroshop-api/app/helper/password"
+	"github.com/BuildWithYou/fetroshop-api/app/helper/responsehelper"
 	"github.com/BuildWithYou/fetroshop-api/app/helper/validatorhelper"
 	appModel "github.com/BuildWithYou/fetroshop-api/app/model"
 	webModel "github.com/BuildWithYou/fetroshop-api/app/modules/web/model"
@@ -13,32 +14,23 @@ import (
 )
 
 func (svc *AuthServiceImpl) Register(ctx *fiber.Ctx) (*appModel.Response, error) {
-	var (
-		message                                        string
-		existingUsername, existingPhone, existingEmail customers.Customer
-	)
+	var existingUsername, existingPhone, existingEmail customers.Customer
 
 	payload := new(webModel.RegistrationRequest)
-	errorMap, err := validatorhelper.ValidateBodyPayload(ctx, svc.Validate, payload)
-	if err != nil {
-		return svc.responseErrorValidation(fiber.Map{"messages": errorMap}), nil
+	errValidation, errParsing := validatorhelper.ValidateBodyPayload(ctx, svc.Validate, payload)
+	if errParsing != nil {
+		return nil, errParsing
 	}
-	/*
-			   TODO:
-		      Add validation and give proper messages:
-		         - Username : required, unique
-		         - Phone : required, unique, numeric
-		         - Email : required, unique, valid email
-		         - FullName : required
-		         - Password : required, min 8
-	*/
+	if errValidation != nil {
+		return responsehelper.ResponseErrorValidation(errValidation), nil
+	}
 
 	result := svc.CustomerRepo.Find(&existingUsername, fiber.Map{"username": payload.Username})
 	if result.Error != nil && !gormhelper.IsErrRecordNotFound(result.Error) {
 		return nil, result.Error
 	}
 	if !gormhelper.IsErrRecordNotFound(result.Error) {
-		return nil, errorhelper.Error400("Username already used") // #marked: message
+		return responsehelper.ResponseErrorValidation(fiber.Map{"username": "Username already used"}), nil // #marked: message
 	}
 
 	result = svc.CustomerRepo.Find(&existingPhone, fiber.Map{"phone": payload.Phone})
@@ -46,7 +38,7 @@ func (svc *AuthServiceImpl) Register(ctx *fiber.Ctx) (*appModel.Response, error)
 		return nil, result.Error
 	}
 	if !gormhelper.IsErrRecordNotFound(result.Error) {
-		return nil, errorhelper.Error400("Phone already used") // #marked: message
+		return responsehelper.ResponseErrorValidation(fiber.Map{"phone": "Phone already used"}), nil // #marked: message
 	}
 
 	result = svc.CustomerRepo.Find(&existingEmail, fiber.Map{"email": payload.Email})
@@ -54,7 +46,7 @@ func (svc *AuthServiceImpl) Register(ctx *fiber.Ctx) (*appModel.Response, error)
 		return nil, result.Error
 	}
 	if !gormhelper.IsErrRecordNotFound(result.Error) {
-		return nil, errorhelper.Error400("Email already used") // #marked: message
+		return responsehelper.ResponseErrorValidation(fiber.Map{"email": "Email already used"}), nil // #marked: message
 	}
 
 	hashedPassword := password.Generate(payload.Password)
@@ -69,14 +61,13 @@ func (svc *AuthServiceImpl) Register(ctx *fiber.Ctx) (*appModel.Response, error)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-
-	if gormhelper.HasAffectedRows(result) {
-		message = "User created successfully" // #marked: message
+	if !gormhelper.HasAffectedRows(result) {
+		return nil, errorhelper.Error500("Failed to create user") // #marked: message
 	}
 
 	return &appModel.Response{
 		Code:    fiber.StatusCreated,
 		Status:  utils.StatusMessage(fiber.StatusCreated),
-		Message: message,
+		Message: "User created successfully", // #marked: message
 	}, nil
 }
