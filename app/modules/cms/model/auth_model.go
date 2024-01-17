42527d13c65f8614de4c8623dfa8473b913771f1
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-01-17 17:24:31 +0700

    Cms: Auth : Refactor register service

--- app/modules/cms/model/auth_model.go
+++ app/modules/cms/model/auth_model.go
@@ -3,9 +3,9 @@ package model
 type RegistrationRequest struct {
 	Username string `json:"username" form:"username" validate:"required"`
 	Phone    string `json:"phone" form:"phone" validate:"required"`
-	Email    string `json:"email" form:"email" validate:"required"`
+	Email    string `json:"email" form:"email" validate:"required,email"`
 	FullName string `json:"fullName" form:"fullName" validate:"required"`
-	Password string `json:"password" form:"password" validate:"required"`
+	Password string `json:"password" form:"password" validate:"required,min=8"`
 }
 
 type LoginRequest struct {
