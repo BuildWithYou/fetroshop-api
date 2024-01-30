37a362dba9d4b1d42533f705cacbbf89613e264d
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-01-30 14:36:07 +0700

    Docker: add container for migrate database test

--- shell/docker-refresh-volumes.sh
+++ shell/docker-refresh-volumes.sh
@@ -1,3 +1,2 @@
 docker compose -f docker-compose.yaml --project-name fetroshop down --volumes
-docker compose -f docker-compose.yaml --project-name fetroshop build --no-cache
 docker compose -f docker-compose.yaml --project-name fetroshop up -d
--- shell/docker-refresh.sh
+++ shell/docker-refresh.sh
@@ -1,2 +1,2 @@
-docker compose -f docker-compose.yaml down
+docker compose -f docker-compose.yaml --project-name fetroshop down
 docker compose -f docker-compose.yaml --project-name fetroshop up -d
